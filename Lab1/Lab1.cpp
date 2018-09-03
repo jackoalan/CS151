937d17a610e8b2e887648cc8e3e483bbfa434723
Jack Andersen <jackoalan@gmail.com>
2018-09-02 18:31:57 -1000

    Initial repository with 2 labs

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,113 @@
+/**
+ * 3n+1 Problem
+ * Jack Andersen
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+namespace Lab1
+{
+
+/**
+ * @brief State for 3n+1 algorithm
+ * Iterative state machine for stepping through 3n+1 algorithm until termination
+ */
+class ThreeNPlusOne
+{
+  int m_value; /**< Current n value */
+public:
+  /**
+   * Initializing constructor for 3n+1 algorithm
+   * @param input initial n value for state machine
+   */
+  explicit ThreeNPlusOne(int input) : m_value(input) {}
+
+  /**
+   * @brief Execute one iteration of 3n+1 algorithm
+   * @return True if further steps remain, False on termination
+   */
+  bool step() {
+    if (m_value == 1)
+      return false;
+    if (m_value & 1) {
+      /* Odd - perform 3n+1 */
+      m_value = m_value * 3 + 1;
+    } else {
+      /* Even - perform n/2 */
+      m_value /= 2;
+    }
+    return true;
+  }
+};
+
+/**
+ * @brief Maximum cycle counter for intervals of 3n+1 trials
+ */
+class Counter
+{
+  int m_maxCycleCount = 0; /**< Maximum cycle tracker */
+public:
+  /**
+   * @brief Run 3n+1 cycles for a specified interval of n values
+   * @param i Minimum n value, inclusive
+   * @param j Maximum n value, inclusive
+   * @return Maximum cycle count
+   */
+  int run(int i, int j) {
+    for (int k = i; k <= j; ++k) {
+      ThreeNPlusOne m(k);
+      int cycleCount = 1; /* Include terminating state */
+      while (m.step()) { ++cycleCount; }
+      m_maxCycleCount = max(m_maxCycleCount, cycleCount);
+    }
+    return m_maxCycleCount;
+  }
+};
+
+/**
+ * Interval of n values
+ */
+struct InputPair
+{
+  int i, j;
+};
+
+}
+
+/**
+ * Driver for running 3n+1 cycle intervals from user input
+ */
+int main(int argc, char** argv) {
+  cout << "Enter 2 values \"i j\" per line, empty line to finish" << endl;
+
+  vector<Lab1::InputPair> inPairs;
+  string s;
+  /* Format on a per-line basis */
+  while (getline(cin, s))
+  {
+    if (s.empty()) {
+      /* Empty line begins executing pairs */
+      for (const Lab1::InputPair& p : inPairs) {
+        Lab1::Counter c;
+        cout << p.i << ' ' << p.j << ' ' << c.run(p.i, p.j) << endl;
+      }
+      break;
+    } else {
+      /* Non-empty line attempts to extract additional pair */
+      istringstream tmp(s);
+      int i, j;
+      tmp >> i >> j;
+      if (tmp.fail()) {
+        cout << "Invalid input format" << endl;
+        return 1;
+      }
+      inPairs.push_back(Lab1::InputPair{i, j});
+    }
+  }
+  return 0;
+}
---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,99 @@
+#include <cstdlib>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "Lab2.hpp"
+
+using namespace std;
+
+namespace Lab2
+{
+
+/**
+ * @brief signed->unsigned cumulative modulo helper with arbitrary divisor
+ * @param quot Reference to pre-set quotient, input may be negative, output may be negative
+ * @param rem Reference to pre-set remainder, input may be negative, output always positive
+ * @param divisor Positive integer serving as divisor
+ */
+static void ModDiv(int& quot, int& rem, int divisor) {
+  auto theDiv = div(rem, divisor);
+  quot += theDiv.quot;
+  if (theDiv.rem < 0) {
+    --quot;
+    rem = theDiv.rem + divisor;
+  } else {
+    rem = theDiv.rem;
+  }
+}
+
+/**
+ * @brief Applies chain of modulo arithmetic to ensure positive attributes
+ */
+void Clock::fixup() {
+  ModDiv(m_minutes, m_seconds, 60);
+  ModDiv(m_hours, m_minutes, 60);
+  int days = 0; /* Clock overflows/underflows into this temporary days component */
+  ModDiv(days, m_hours, 24);
+}
+
+/**
+ * @brief Outputs formatted clock to ostream
+ */
+std::ostream& operator<<(std::ostream& out, const Clock& clk) {
+  return out << right << setfill('0') <<
+         setw(2) << clk.m_hours << ':' <<
+         setw(2) << clk.m_minutes << ':' <<
+         setw(2) << clk.m_seconds;
+}
+
+/**
+ * @brief Applies extracted stream values to clock, no change on failure
+ */
+std::istream& operator>>(std::istream& in, Clock& clk) {
+  Clock newClock;
+  in >> newClock.m_hours >> newClock.m_minutes >> newClock.m_seconds;
+  if (in.fail())
+    return in;
+  clk = newClock;
+  clk.fixup();
+  return in;
+}
+
+}
+
+/**
+ * Driver for running clock with user input
+ */
+int main(int argc, char** argv) {
+  cout << "Enter 3 values \"h m s\" per line to set clock," << endl <<
+  "'a' to spring ahead, 'b' to fall back, empty line to exit" << endl;
+
+  Lab2::Clock clk;
+  string s;
+  /* Format on a per-line basis */
+  while (getline(cin, s))
+  {
+    if (s.empty()) {
+      /* Empty line exits */
+      break;
+    } else if (s[0] == 'a') {
+      /* 'a' for spring ahead */
+      clk.springAhead();
+    } else if (s[0] == 'b') {
+      /* 'b' for fall back */
+      clk.fallBack();
+    } else {
+      /* Non-empty line attempts to extract time components */
+      istringstream tmp(s);
+      tmp >> clk;
+      if (tmp.fail()) {
+        cout << "Invalid input format" << endl;
+        continue;
+      }
+    }
+    cout << clk << endl;
+  }
+  return 0;
+}
